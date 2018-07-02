db032ad4eb17ec2ca241ad9d14bce31a7b24e92f
selahadiku <34514711+selahadiku@users.noreply.github.com>
2018-07-02 10:10:49 +0700

    Update hadi.js

--- hadi.js
+++ hadi.js
@@ -1,2 +1,59 @@
-function genieeInitInpageBanner(e,t,i,o,a,n){for(var l=["vtc.vn","tamsusao.com"],c=0;c<l.length;c++)if(-1!=window.location.hostname.indexOf(l[c]))return consogule.log("Domain blocked"),!1;if(!window.mobileAndTabletcheck())return!1;var s=document.createElement("div"),d=document.createElement("div"),r=document.querySelector(i),p=Math.floor(r.children.length/2),m=r.children.item(p),g=document.createElement("script"),w=document.createElement("ins"),b=a||300,h=n||600;o&&(m=document.querySelector(o)),g.src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js",d.appendChild(g),w.className="adsbygoogle",w.style.display="inline-block",w.style.width=b+"px",w.style.height=h+"px",w.dataset.adClient=e,w.dataset.adSlot=t,d.appendChild(w),s.style.overflow="hidden",s.style.position="relative",s.style.zIndex=2,s.style.width="100%",s.style.height=h+"px";var u=(window.innerHeight-h)/2,v=(window.innerWidth-b)/2;d.style.clip="rect(0 "+b+"px 0 0px)",d.style.position="fixed",d.style.zIndex="10000",d.style.left=v+"px",d.style.top=u+"px",d.style.transition="0ms",s.appendChild(d),r.insertBefore(s,m),(adsbygoogle=window.adsbygoogle||[]).push({}),document.onscroll=function(){var e=s.getBoundingClientRect().top-u;d.style.clip=e>=0&&e<=h?"rect("+e+"px "+b+"px "+h+"px 0px)":e<0&&e>=-h?"rect(0 "+b+"px "+(h+e)+"px 0px)":"rect(0 "+b+"px 0 0px)"}}window.mobileAndTabletcheck=function(){var e=!1;return function(t){(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(t)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(t.substr(0,4)))&&(e=!0)}(navigator.userAgent||navigator.vendor||window.opera),e};
-I1L0LD0In/aRank157KAgen/al0+1n/awhoissourceMore dataSummary reportDiagnosisDensity00n/a
+<div id="disqus_thread"></div>
+
+<script>
+  window.addEventListener('message', receiveMessage, false);
+
+  function receiveMessage(event) {
+    if (event.data) {
+      var msg;
+      try {
+        msg = JSON.parse(event.data);
+      } catch (err) {}
+      if (!msg)
+        return false;
+      if (msg.name === 'resize') {
+        window.parent.postMessage({
+          sentinel: 'amp',
+          type: 'embed-size',
+          height: msg.data.height
+        }, '*');
+      }
+    }
+  }
+
+  function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split('&');
+    for (var i = 0; i < vars.length; i++) {
+      var pair = vars[i].split('=');
+      if (pair[0] == variable) {
+        return pair[1];
+      }
+    }
+    return (false);
+  }
+
+  function generateCss(q) {
+    var css       = document.createElement('style'),
+        sp2       = document.getElementById('disqus_thread'),
+        parentDiv = sp2.parentNode;
+    css.type = 'text/css';
+    if (css.styleSheet) css.styleSheet.cssText = q;
+    else css.appendChild(document.createTextNode(q));
+    parentDiv.insertBefore(css, sp2);
+  }
+
+  var disqus_config = function() {
+    this.page.url = getQueryVariable('url');
+  };
+
+  (function() {
+    var d = document,
+        s = d.createElement('script'),
+        q = '#disqus_thread {font-family:' + getQueryVariable('fontBodyFamily') + '} a {color:#' + getQueryVariable('fontLinkColor') + '}';
+        generateCss(q);
+    s.src = '//' + getQueryVariable('shortname') + '.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
+  })();
+</script>
